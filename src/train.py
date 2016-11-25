9dba3b42c3840556bfec6086285c6057d0d7fde6
Daiki.Sanno <daiki.sanno@gmail.com>
2016-11-26 00:43:44 +0900

    [fix] Adjust contrast of reconstraction of original image.

--- src/train.py
+++ src/train.py
@@ -201,7 +201,9 @@ def train(args, image_path, source_image_paths, target_image_paths, input_clip_r
     for j in six.moves.range(600):
         losses = update(net, optimizer, link, org_layers, tv_weight)
         if (j + 1) % 20 == 0:
-            residuals.append(cuda.to_cpu(link.x.data) - image)
+            z = cuda.to_cpu(link.x.data)
+            z = adjust_color_distribution(z, image_mean, image_std)
+            residuals.append(z - image)
     for i in six.moves.range(1, 6):
         w = i * 0.1
         print('Generating image for weight: {0:.2f}'.format(w))
