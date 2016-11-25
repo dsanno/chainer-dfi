1a716477c065bc9432b2d080505bbd5d1245fc4d
Daiki.Sanno <daiki.sanno@gmail.com>
2016-11-25 22:36:15 +0900

    [imp] Add feature option to script for LFW dataset.

--- src/train_lfw.py
+++ src/train_lfw.py
@@ -4,12 +4,16 @@ import os
 
 from train import train
 
+attribute_names = 'Male,Asian,White,Black,Baby,Child,Youth,Middle_Aged,Senior,Black_Hair,Blond_Hair,Brown_Hair,Bald,No_Eyewear,Eyeglasses,Sunglasses,Mustache,Smiling,Frowning,Chubby,Blurry,Harsh_Lighting,Flash,Soft_Lighting,Outdoor,Curly_Hair,Wavy_Hair,Straight_Hair,Receding_Hairline,Bangs,Sideburns,Fully_Visible_Forehead,Partially_Visible_Forehead,Obstructed_Forehead,Bushy_Eyebrows,Arched_Eyebrows,Narrow_Eyes,Eyes_Open,Big_Nose,Pointy_Nose,Big_Lips,Mouth_Closed,Mouth_Slightly_Open,Mouth_Wide_Open,Teeth_Not_Visible,No_Beard,Goatee,Round_Jaw,Double_Chin,Wearing_Hat,Oval_Face,Square_Face,Round_Face,Color_Photo,Posed_Photo,Attractive_Man,Attractive_Woman,Indian,Gray_Hair,Bags_Under_Eyes,Heavy_Makeup,Rosy_Cheeks,Shiny_Skin,Pale_Skin,5_o\'_Clock_Shadow,Strong_Nose-Mouth_Lines,Wearing_Lipstick,Flushed_Face,High_Cheekbones,Brown_Eyes,Wearing_Earrings,Wearing_Necktie,Wearing_Necklace'.lower().split(',')
+attribute_ids = {k:i for i, k in enumerate(attribute_names)}
+
 def parse_arg():
     parser = argparse.ArgumentParser('Deep Feature Interpolation using LFW dataset')
     parser.add_argument('image_dir', type=str, help='Image directory path')
     parser.add_argument('attr_file', type=str, help='Attribute file path')
     parser.add_argument('input_name', type=str, help='Input person name')
     parser.add_argument('input_index', type=int, help='Input image index')
+    parser.add_argument('feature', type=str, help='Feature name')
     parser.add_argument('output_image', type=str, help='Output image file path')
     parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU device index (negative value indicate CPU)')
     parser.add_argument('--model', '-m', type=str, default='vgg19.model', help='Model file path')
@@ -58,8 +62,10 @@ def main():
     args = parse_arg()
     attribute_dataset = load_attribute_dataset(args.attr_file)
     attribute = find_attribute(attribute_dataset, args.input_name, args.input_index)
-    # TODO args
-    attribute_id = 17
+    if not args.feature in attribute_names:
+        print('Error: {} is invalid attribute'.format(args.feature))
+        exit()
+    attribute_id = attribute_ids[args.feature]
     source_indices = nearest_attributes(attribute_dataset, attribute, attribute_id, False, args.near_image)
     target_indices = nearest_attributes(attribute_dataset, attribute, attribute_id, True, args.near_image)
     source_paths = image_paths(args.image_dir, attribute_dataset, source_indices)
