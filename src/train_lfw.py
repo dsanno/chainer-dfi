57826d867974917911a897c2a58350c310b879ea
Daiki.Sanno <daiki.sanno@gmail.com>
2016-12-01 07:20:25 +0900

    [imp] Support multiple features interpolation for LFW dataset.

--- src/train_lfw.py
+++ src/train_lfw.py
@@ -13,7 +13,7 @@ def parse_arg():
     parser.add_argument('attr_file', type=str, help='Attribute file path')
     parser.add_argument('input_name', type=str, help='Input person name')
     parser.add_argument('input_index', type=int, help='Input image index')
-    parser.add_argument('feature', type=str, help='Feature name. Words must be concatenated with "_", and leading with "~" indicates set feature off (e.g. ~no_beard)')
+    parser.add_argument('feature', type=str, help='Feature name. Words must be concatenated with "_", and leading with "~" indicates set feature off (e.g. ~no_beard). You can use multiple features with comma separated feature names (e.g. ~mouth_closed,smiling)')
     parser.add_argument('output_image', type=str, help='Output image file path')
     parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU device index (negative value indicate CPU)')
     parser.add_argument('--model', '-m', type=str, default='vgg19.model', help='Model file path')
@@ -42,13 +42,19 @@ def image_paths(image_dir, attribute_dataset, indices):
     names, numbers, attributes = attribute_dataset
     return [make_image_path(image_dir, names[i], numbers[i]) for i in indices]
 
-def nearest_attributes(attribute_dataset, attribute, attribute_id, positive, nearest_num):
+def nearest_attributes(attribute_dataset, attribute, feature_flags, nearest_num, reverse=False):
     names, numbers, attributes = attribute_dataset
     indices = np.arange(len(names))
-    if positive:
-        selected = attributes[:,attribute_id] >= 0.5
-    else:
-        selected = attributes[:,attribute_id] < -0.5
+    selected = True
+    for feature, on in feature_flags:
+        attribute_id = attribute_ids[feature]
+        if reverse:
+            on = not on
+        if on:
+            feature_selected = attributes[:,attribute_id] >= 0.5
+        else:
+            feature_selected = attributes[:,attribute_id] < -0.5
+        selected = np.logical_and(selected, feature_selected)
     distance = np.linalg.norm(attributes[selected] - attribute, axis=1)
     tops = np.argsort(distance)[:nearest_num]
     return np.arange(attributes.shape[0])[selected][tops]
@@ -62,17 +68,19 @@ def main():
     args = parse_arg()
     attribute_dataset = load_attribute_dataset(args.attr_file)
     attribute = find_attribute(attribute_dataset, args.input_name, args.input_index)
-    feature = args.feature.lower()
-    feature_on = True
-    if feature.startswith('~'):
-        feature = feature[1:]
-        feature_on = False
-    if not feature in attribute_names:
-        print('Error: {} is invalid attribute'.format(feature))
-        exit()
-    attribute_id = attribute_ids[feature]
-    source_indices = nearest_attributes(attribute_dataset, attribute, attribute_id, not feature_on, args.near_image)
-    target_indices = nearest_attributes(attribute_dataset, attribute, attribute_id, feature_on, args.near_image)
+    features = args.feature.lower().split(',')
+    feature_flags = []
+    for feature in features:
+        if feature.startswith('~'):
+            feature = feature[1:]
+            feature_flags.append((feature, False))
+        else:
+            feature_flags.append((feature, True))
+        if not feature in attribute_names:
+            print('Error: {} is invalid attribute'.format(feature))
+            exit()
+    source_indices = nearest_attributes(attribute_dataset, attribute, feature_flags, args.near_image, reverse=True)
+    target_indices = nearest_attributes(attribute_dataset, attribute, feature_flags, args.near_image)
     source_paths = image_paths(args.image_dir, attribute_dataset, source_indices)
     target_paths = image_paths(args.image_dir, attribute_dataset, target_indices)
     image_path = make_image_path(args.image_dir, args.input_name, args.input_index)
